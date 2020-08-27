d8f8bf2d49d23c1a4ac4b1a0ca69c0b13ac0135f
taesikyoo <yrns0907@gmail.com>
2020-08-27 17:56:56 +0900

    JavaMailSender와 Gmail SMTP Server를 이용한 메일 발송

---
+++ src/main/java/com/example/springmail/controller/MailController.java
@@ -0,0 +1,25 @@
+package com.example.springmail.controller;
+
+import com.example.springmail.dto.MailDto;
+import com.example.springmail.service.MailService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@AllArgsConstructor
+@Controller
+public class MailController {
+
+    private final MailService mailService;
+
+    @GetMapping("/mail")
+    public String dispMail() {
+        return "mail";
+    }
+
+    @PostMapping("/mail")
+    public void execMail(MailDto mailDto) {
+        mailService.mailSend(mailDto);
+    }
+}
